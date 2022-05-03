f1161e51b85431fc4ab7139726ccfde9ce86fd9d
AKallai <alex.kallai@dmgmori-heitec.hu>
2022-05-03 12:05:25 +0200

    hazi

---
+++ 2.alkalom_inventory_hw.py
@@ -0,0 +1,47 @@
+
+class InventoryBase:
+    """
+    This class can be used as a basic inventory
+    """
+
+    def __init__(self):
+        self.storageDict = {"initialized_item":1}
+
+    def addItem(self, name, amount):
+        self.storageDict.update({str(name):int(amount)})
+        print(f"added {name} record with {amount} amount")
+
+    def checkOutItem(self, name, amount):
+        self.storageDict[name] -= amount
+        print(f"'{name}' record was decreased by {amount} in the inventory.")
+
+    def listItems(self):
+        print("\nThe items in the inventory are:")
+        for listItem in self.storageDict:
+            print(f"{listItem} -- {self.storageDict[listItem]}")
+        print("END\n")
+        pass
+
+    def deleteItem(self, name):
+        self.storageDict.pop(name)
+        print(f"'{name}' record was deleted from the inventory.")
+
+
+def main():
+    # Instantiate the class
+    inventory = InventoryBase()
+
+    inventory.listItems()
+
+    inventory.addItem("kalapacs", 6)
+    inventory.addItem("veso", 3)
+    inventory.listItems()
+
+    inventory.checkOutItem("veso", 2)
+    inventory.listItems()
+
+    inventory.deleteItem("veso")
+    inventory.listItems()
+
+if __name__ == "__main__":
+    main()
